434a92f01a7780f1d61e34a3c26688ebb34339b5
TheCap213 <167722765+TheCap213@users.noreply.github.com>
2025-06-10 19:24:12 -0500

    Correcciones del menu principal

--- TF_OpenLibrary_Grupo09/Colores.hpp
+++ TF_OpenLibrary_Grupo09/Colores.hpp
@@ -43,7 +43,8 @@ void clearBackground(int r, int g, int b, int alto = 25, int ancho = 80) {
 	cout << "\x1b[" << alto << "A"; // Subir cursor
 }
 
-void dibujarBordes() {
+void dibujarBordes(const string& hexColor) {
+	SetConsoleOutputCP(CP_UTF8);
 	HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_SCREEN_BUFFER_INFO infoConsola;
 	GetConsoleScreenBufferInfo(consola, &infoConsola);
@@ -51,17 +52,15 @@ void dibujarBordes() {
 	int ancho = infoConsola.srWindow.Right - infoConsola.srWindow.Left + 1;
 	int alto = infoConsola.srWindow.Bottom - infoConsola.srWindow.Top + 1;
 
-	char bloque = char(219);
+	string bloque = u8"█"; // Unicode block
+	textColor(hexColor);
 
-	// Guardar la posición actual del cursor
 	COORD cursorOriginal = infoConsola.dwCursorPosition;
 
-	// Línea superior
 	COORD filaSuperior = { 0, infoConsola.srWindow.Top };
 	SetConsoleCursorPosition(consola, filaSuperior);
 	for (int i = 0; i < ancho; ++i) cout << bloque;
 
-	// Columnas laterales
 	for (int fila = infoConsola.srWindow.Top + 1; fila < infoConsola.srWindow.Bottom; ++fila) {
 		COORD izquierda = { 0, (SHORT)fila };
 		COORD derecha = { (SHORT)(ancho - 1), (SHORT)fila };
@@ -70,13 +69,12 @@ void dibujarBordes() {
 		SetConsoleCursorPosition(consola, derecha);   cout << bloque;
 	}
 
-	// Línea inferior
 	COORD filaInferior = { 0, infoConsola.srWindow.Bottom };
 	SetConsoleCursorPosition(consola, filaInferior);
 	for (int i = 0; i < ancho; ++i) cout << bloque;
 
-	// Restaurar cursor original
 	SetConsoleCursorPosition(consola, cursorOriginal);
+	resetColor();
 }
 
 void posicion(int x, int y) {
--- TF_OpenLibrary_Grupo09/MenuFuncionalidad.hpp
+++ TF_OpenLibrary_Grupo09/MenuFuncionalidad.hpp
@@ -1,52 +1,76 @@
 ﻿#pragma once
 #include "Colores.hpp"
 #include "FigurasMenu.hpp"
+#include <ctime>
+
+void leerOpcionValida(int& opcion, int min, int max);
+int numRandom();
 
 void menuPrincipal() {
-    
-	int opcion;
-
-	do {
-	system("title OpenLibrary");	//Titulo de la consola
+	system("cls");
+	int opcion; 
 	
-	//Diseño del menu principal.
-	textColor("#b3b3b3");
-	dibujarBordes();
-	resetColor();
-
-    tituloMenu(20, 6);
+	system("tittle Open Library");
+	dibujarBordes("#b3b3b3");
+	tituloMenu(20, 6); 
 	birrete(16, 15);
 	birrete(88, 15);
-	libroMenu(27, 26);
-
+	libroMenu(27, 27);
+	
 	backgroundColor("#41b68f");
 	textColor("#000000");
 	posicion(43, 15); cout << "|.......... BIENVENIDO ..........|";
 	resetColor();
 
 	textColor("#ffffff");
-	posicion(45, 17); cout << "> 1. Resgistrar nuevo usuario";
-	posicion(45, 19); cout << "> 2. Iniciar sesion";
-	posicion(45, 21); cout << "> 3. Salir";
-	posicion(43, 23); cout << "> Seleccione una opcion: ";
-	posicion(68, 23); cin >> opcion;
+	posicion(45, 17); cout << "1. Resgistrar nuevo usuario";
+	posicion(45, 19); cout << "2. Iniciar sesion";
+	posicion(45, 21); cout << "3. Salir";
+	posicion(43, 23); cout << "> Ingrese una opcion: ";
+	posicion(65, 23); cin >> opcion;
 	resetColor();
 
 
-	} while (opcion != 3);
+	switch (opcion) {
+	case 1 :
+		posicion(43, 25); cout << "Accediendo al modulo de registro..."; 
+		Sleep(numRandom() * 1000); 
+		//COLOCAR LA FUNCION REGISTRO Xd
+		break; 
 
+	case 2: 
+		posicion(43, 25); cout << "Accediendo al modulo de inicio de sesion...";
+		Sleep(numRandom() * 1000);
+		//COLOCAR LA FUNCION INICIO DE SESION
+		break;
 
+	case 3: 
+		posicion(43, 25); cout << "Cerrando Sesion...";
+		Sleep(numRandom() * 1000);
+		break;
+
+	default:
+		posicion(43, 25); cout << "Eliga una opcion valida.";
+		Sleep(numRandom() * 500);
+		menuPrincipal();
+		break;
+	}
 
 }
 
-void MenuInicioSesion() {
-	
-
+// Pendiente su retiro de esta cosa xd
+void leerOpcionValida(int& opcion, int min, int max) {
+	while (cin.fail() || opcion < min || opcion > max) {
+		cin.clear();
+		cin.ignore(1000, '\n'); 
+		posicion(43, 25); cout << "Opcion no valida, Intente nuevamente";
+		cin >> opcion;
+	}
 }
 
-void MenuUsuario() {
-
+int numRandom() {
+	srand(time(0)); 
+	int tiempoAleatorio = rand() % 3 + 1;
+	return tiempoAleatorio;
 }
 
-void MenuBibliotecario(){
-}
