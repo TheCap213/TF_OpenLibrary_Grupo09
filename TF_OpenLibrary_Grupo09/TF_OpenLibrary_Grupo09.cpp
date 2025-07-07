b80025106f2bfc2819b7ed39c3ecdb1aae0221a7
TheCap213 <167722765+TheCap213@users.noreply.github.com>
2025-07-06 22:46:54 -0500

    Tamaño de la ventana corregido

--- TF_OpenLibrary_Grupo09/TF_OpenLibrary_Grupo09.cpp
+++ TF_OpenLibrary_Grupo09/TF_OpenLibrary_Grupo09.cpp
@@ -5,9 +5,21 @@
 int DVD::idCounter = 0;
 int Libro::idCounter = 0;
 
-int main() {
-	//system("color fd");
+//En caso no compile eliminar esto (solo para el tamaño del programa)
+void bloquearResize() {
+    HWND hwnd = GetConsoleWindow();
+    LONG style = GetWindowLong(hwnd, GWL_STYLE);
+    style &= ~WS_MAXIMIZEBOX;
+    style &= ~WS_SIZEBOX;
+    SetWindowLong(hwnd, GWL_STYLE, style);
+}
 
+int main() {
+    //En caso no compile eliminar estas 2 funciones.
+    system("mode con: cols=128 lines=37");
+    bloquearResize();
+
+    //parte princiapl
 	menuPrincipal();
 
 	_getch();
